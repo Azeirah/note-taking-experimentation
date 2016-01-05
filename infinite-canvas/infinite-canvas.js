b3a5cbc68dd16e7cb73f97aac4ef44226da26f70
Martijn Brekelmans <tijntje_7@msn.com>
2016-01-05 19:55:11 +0100

    Cleaned up implementation some more, added moveTo function and added render options to moveBy&moveTo

--- infinite-canvas/infinite-canvas.js
+++ infinite-canvas/infinite-canvas.js
@@ -4,14 +4,6 @@
         chunkHeight: 216
     };
 
-    // we need to get rid of this..
-    Math.constrain = function constrain(value, minimum, maximum) {
-        "use strict";
-        if (value < minimum) return minimum;
-        if (value > maximum) return maximum;
-        return value;
-    };
-
     function initializeWorld(ctx) {
         var offscreenRenderCanvas    = document.createElement("canvas");
         var offscreenRenderCtx       = offscreenRenderCanvas.getContext("2d");
@@ -34,6 +26,7 @@
         };
 
         function constructChunkKey(x, y) {
+            // chunk keys look like "0, 1"
             return x.toString() + ", " + y.toString();
         }
 
@@ -123,9 +116,9 @@
             // we need to figure out what chunks are in the viewport
             // what we need to do is find the coordinates of the chunks in the four corners
             // and then retrieve those chunks, as well as all chunks inbetween those corners
-            var topLeft     = worldCoordToChunkCoord(infinity.position.x, infinity.position.y);
+            var topLeft     = worldCoordToChunkCoord(infinity.position.x,                infinity.position.y);
             var topRight    = worldCoordToChunkCoord(infinity.position.x + canvas.width, infinity.position.y);
-            var bottomLeft  = worldCoordToChunkCoord(infinity.position.x, infinity.position.y + canvas.height);
+            var bottomLeft  = worldCoordToChunkCoord(infinity.position.x,                infinity.position.y + canvas.height);
             var bottomRight = worldCoordToChunkCoord(infinity.position.x + canvas.width, infinity.position.y + canvas.height);
 
             var chunksOnXAxis = Math.abs(topRight.x - topLeft.x);
@@ -134,9 +127,7 @@
             // <= instead of < because we definitely need to include the outer layer of chunks as well!
             for (var x = 0; x <= chunksOnXAxis; x++) {
                 for (var y = 0; y <= chunksOnYAxis; y++) {
-                    // at this point I wish javascript dictionaries were like python dicts
-                    // that can take immutable tuples as keys ;_;
-                    var chunkKey = constructChunkKey(topLeft.x + x, topLeft.y + y);
+                    var chunkKey               = constructChunkKey(topLeft.x + x, topLeft.y + y);
                     chunksInViewport[chunkKey] = getChunk(topLeft.x + x, topLeft.y + y);
                 }
             }
@@ -150,27 +141,15 @@
             Object.keys(chunks).forEach(function (key) {
                 var coord           = parseChunkKey(key);
                 var renderCoord     = chunkCoordToRenderCoord(coord.x, coord.y);
-                var chunk           = chunks[key];
                 var chunkWorldCoord = chunkCoordToWorldCoord(coord.x, coord.y);
 
+                var chunkSourceCoord = {
+                    x: Math.max(renderCoord.x, 0),
+                    y: Math.max(renderCoord.y, 0)
+                }
 
-                // A and D are only used to calculate the -clipped- width and height of a chunk, I am sure there's a far
-                // easier way to calculate the clipped width and height of a chunk
-                // but for now, this works, so it'll do
-
-                // top-left
-                var A = {
-                    x: Math.constrain(renderCoord.x, 0, canvas.width),
-                    y: Math.constrain(renderCoord.y, 0, canvas.height)
-                };
-                // bottom-right
-                var D = {
-                    x: Math.constrain(renderCoord.x + configuration.chunkWidth,  0, canvas.width),
-                    y: Math.constrain(renderCoord.y + configuration.chunkHeight, 0, canvas.height)
-                };
-
-                var width  = D.x - A.x;
-                var height = D.y - A.y;
+                var width  = Math.min(renderCoord.x + configuration.chunkWidth , canvas.width)  - chunkSourceCoord.x;
+                var height = Math.min(renderCoord.y + configuration.chunkHeight, canvas.height) - chunkSourceCoord.y;
 
                 // don't even bother to update chunks that are not even visible on the canvas! :o
                 if (width <= 0 || height <= 0) return;
@@ -189,12 +168,12 @@
                 }
 
                 // ..simply load the chunk into the render context, nothing special here
-                offscreenRenderCtx.putImageData(chunk, 0, 0);
+                offscreenRenderCtx.putImageData(chunks[key], 0, 0);
                 // ..now get the corresponding data from the canvas and put it into the chunk
                 // clear the chunk beforehand, otherwise transparent pixels (anti-aliasing) will accumulate
                 // resulting in ugly thick lines
                 offscreenRenderCtx.clearRect(putLocation.x, putLocation.y, width, height);
-                offscreenRenderCtx.drawImage(canvas, A.x, A.y, width, height, putLocation.x, putLocation.y, width, height);
+                offscreenRenderCtx.drawImage(canvas, chunkSourceCoord.x, chunkSourceCoord.y, width, height, putLocation.x, putLocation.y, width, height);
                 // ..overwrite the storage chunk to the just rendered one
                 infinity.chunks[key] = offscreenRenderCtx.getImageData(0, 0, configuration.chunkWidth, configuration.chunkHeight);
                 // createImageBitmap gives us a HUGE performance increase during padding, use it if it's available
@@ -209,13 +188,29 @@
             });
         };
 
-        infinity.moveBy = function(dx, dy, canvas) {
+        infinity.moveBy = function(dx, dy, render) {
+            // default render to true, only skip rendering when it's false
+            render = render === undefined? true : render;
             infinity.position.x += dx;
             infinity.position.y += dy;
 
-            var chunksToRender = getChunksInViewport();
-            renderChunks(chunksToRender);
+            if (render) {
+                var chunksToRender = getChunksInViewport();
+                renderChunks(chunksToRender);
+            }
         };
+
+        infinity.moveTo = function(x, y, render) {
+            // default render to true, only skip rendering when it's false
+            render = render === undefined? true : render;
+            infinity.position.x = x;
+            infinity.position.y = y;
+
+            if (render) {
+                var chunksToRender = getChunksInViewport();
+                renderChunks(chunksToRender);
+            }
+        }
 
         return infinity;
     }
